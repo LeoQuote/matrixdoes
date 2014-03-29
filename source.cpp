3f468963c881dcee4376ee991e9b379c825f8f16
LeoQuote <cnmwozhendebuzhidao@gmail.com>
2014-03-29 17:25:56 +0800

    修改source .cpp 尝试返回数组，已失败 是时候学习指针了

--- source.cpp
+++ source.cpp
@@ -1,28 +1,30 @@
 #include<stdio.h>
 #include<conio.h>
-int main()
+float main()
 {
 	int a,i,j,k;
-	printf("hehe\n");
+//	printf("hehe\n"); 
 	float b[4][4]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
 	for (k=0;k<3;k++){
 		for (i=k+1;i<4;i++) {
 			for (j=k;j<4;j++) {
 			b[i][j]=b[i][j]-b[i][k]*b[k][j]/b[k][k];
-/*				printf ("%f ",b[i][j]);*/
+			b[i][j]=b[i][j]/b[i][i];
+//				printf ("%f ",b[i][j]);
 			}
-			printf("\n");
+//			printf("\n");
 		}
-		printf("\n");
+//		printf("\n");
 		/*打印矩阵 */
 		for (i=0;i<4;i++) {
 			for (j=0;j<4;j++) {
 	/*			b[i][j]=b[i][j]-b[i][1]*b[1][j]/b[1][1]; */
-				printf ("%f ",b[i][j]);
+//				printf ("%f ",b[i][j]);
 			}
-			printf("\n");
+//			printf("\n");
 		}
 	}
-	printf("Press any key to continue...");
-	getch();
+//	return b; 如何让函数返回一个数组呢。。。。。
+//	printf("Press any key to continue...");
+/*	getch(); */
 }
